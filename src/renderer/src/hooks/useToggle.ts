731df775f332df2c7749bbaa4898d0a3bf476301
lizlo <francisco.lucas0507@gmail.com>
2023-03-17 13:43:05 -0300

    parameters for the toggle hook

--- src/renderer/src/hooks/useToggle.ts
+++ src/renderer/src/hooks/useToggle.ts
@@ -3,7 +3,7 @@ import {useState} from 'react'
 export const useToggle = (intialState: boolean): [boolean, () => void] => {
   const [toggle, setToggle] = useState(intialState)
 
-  const toggleValue = () => setToggle((t) => !t)
+  const toggleValue = (value?: boolean) => setToggle((t) => value || !t)
 
   return [toggle, toggleValue]
 }
