117c2f89809f81d1770853bc8b3090a08e09d1b8
lucas <francisco.lucas0507@gmail.com>
2023-04-29 15:00:24 -0300

    Progress on the custom video player

--- src/renderer/src/components/VideoPlayer.tsx
+++ src/renderer/src/components/VideoPlayer.tsx
@@ -1,18 +1,20 @@
-function VideoPlayer(
-  props: any,
-  contentProps: HTMLAttributes<HTMLImageElement | HTMLVideoElement> | undefined,
-  uri: string,
-) {
+import {useHotkeys} from '@renderer/hooks/useHotkeys'
+import clsx from 'clsx'
+import {HTMLAttributes, useState, useRef} from 'react'
+
+export function VideoPlayer({
+  uri,
+  contentProps,
+  controls,
+  ...props
+}: HTMLAttributes<HTMLDivElement> & {
+  uri: string
+  controls?: boolean
+  contentProps?: HTMLAttributes<HTMLImageElement | HTMLVideoElement> | undefined
+}) {
   const [prog, setProg] = useState(0)
   const videoRef = useRef<HTMLVideoElement>(null)
   const [headPos, setHeadPos] = useState(0)
-
-  useHotkeys({
-    space: () =>
-      videoRef.current?.paused
-        ? videoRef.current?.play()
-        : videoRef.current?.pause(),
-  })
 
   return (
     <div
@@ -39,9 +41,18 @@ function VideoPlayer(
         }}
       ></video>
 
-      <div className='absolute bottom-0 flex w-full translate-y-full flex-row bg-black bg-opacity-50 px-10 text-white transition-transform group-hover/player:translate-y-0'>
+      <div
+        className={clsx(
+          'absolute bottom-0 flex w-full translate-y-full flex-row bg-black bg-opacity-50 px-10 text-white transition-transform group-hover/player:translate-y-0',
+          controls ? '' : 'hidden',
+        )}
+        onClick={(evt) => {
+          evt.preventDefault()
+          evt.stopPropagation()
+        }}
+      >
         <span
-          className='pointer-events-auto font-mono transition-colors hover:bg-red-500'
+          className='pointer-events-auto flex items-center px-2 align-middle font-mono transition-colors hover:bg-red-500'
           onClick={(evt) => {
             evt.stopPropagation()
             if (videoRef.current!.paused) {
@@ -92,7 +103,10 @@ function VideoPlayer(
             }}
           ></div>
         </div>
-        VIDEO PLAYER FOOTER
+        <span className='font-mono text-opacity-20'>TAGGER PLAYER V0.001</span>
+        <span className='pointer-events-auto flex items-center px-2 align-middle font-mono text-opacity-20 transition-colors hover:bg-red-500'>
+          ⬜
+        </span>
       </div>
 
       <span className='absolute inset-1/2 z-20 font-mono text-white text-opacity-100'>
