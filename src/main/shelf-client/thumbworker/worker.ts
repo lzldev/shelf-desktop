c52222bf751d78ee163cc04bc75781f783a8efe9
Lucas Costa <francisco.lucas0507@gmail.com>
2023-10-19 11:09:52 -0300

    thumbworker resize

--- src/main/shelf-client/thumbworker/worker.ts
+++ src/main/shelf-client/thumbworker/worker.ts
@@ -7,9 +7,10 @@ import {
   workerData as _workerData,
 } from 'node:worker_threads'
 
-import {createWorkerLogger} from '../../utils/Loggers'
-import {ThumbWorkerDataParser, ThumbWorkerInvoke} from './types'
-import {handleWorkerMessage} from '../ai_worker/types'
+import { createWorkerLogger } from '../../utils/Loggers'
+import { ThumbWorkerDataParser, ThumbWorkerInvoke } from './types'
+import { handleWorkerMessage } from '../ai_worker/types'
+import { join } from 'node:path'
 
 const workerData = ThumbWorkerDataParser.parse(_workerData)
 
@@ -28,6 +29,35 @@ async function main() {
   setInterval(() => {
     LOGGER.info('Miau')
   }, 5000)
+
+  handleWorkerMessage<ThumbWorkerInvoke>(port, {
+    resize_image: async ({ data }) => {
+      LOGGER.info(`resizing ${data.filePath}`)
+
+      let image
+      try {
+        image = sharp(data.filePath, {
+          failOn: 'none',
+        }).resize({
+          width: 300,
+          withoutEnlargement: true,
+        })
+      } catch (e) {
+        LOGGER.error(`error on ${data.filePath} ${JSON.stringify(e)}`)
+        throw e
+      }
+
+      const out_path = join(workerData.thumbnailPath, data.hash + '.jpg')
+
+      LOGGER.info(`writing image to ${out_path}`)
+
+      await image.toFile(out_path).catch((e) => {
+        LOGGER.error(`error on ${data.filePath} ${JSON.stringify(e)}`)
+      })
+
+      //TODO: Send response to MAIN thread
+    },
+  })
 }
 
 main()
